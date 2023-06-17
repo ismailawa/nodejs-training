c0fde42aef9c9e335ae81499d60d765d673587e4
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2023-06-17 15:34:43 +0100

    add courses

--- helpers/connection.js
+++ helpers/connection.js
@@ -9,12 +9,26 @@ connection.connect((error) => {
   } else {
     console.log(`Connection successfully`);
     connection.query(
-      'CREATE TABLE IF NOT EXISTS users (id INTEGER(11) PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20) NOT NULL, email VARCHAR(50) NOT NULL,phone VARCHAR(20) NOT NULL, address VARCHAR(100) NOT NULL)',
+      'CREATE TABLE IF NOT EXISTS users (id INTEGER(11) PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20) NOT NULL, email VARCHAR(50) NOT NULL UNIQUE,phone VARCHAR(20) NOT NULL, address VARCHAR(100) NOT NULL)',
       (err, result) => {
         if (err) {
-          console.log(`Create database failed: ${err.message}`);
+          console.log(`Create users table failed: ${err.message}`);
         } else {
-          console.log(`Create database successfull: ${result}`);
+          console.log(
+            `Users table created successfull: ${result.affectedRows}`
+          );
+        }
+      }
+    );
+    connection.query(
+      'CREATE TABLE IF NOT EXISTS courses (id INTEGER(11) PRIMARY KEY AUTO_INCREMENT, title VARCHAR(50) NOT NULL UNIQUE, code VARCHAR(10) NOT NULL UNIQUE,credit_unit FLOAT(2,1) NOT NULL)',
+      (err, result) => {
+        if (err) {
+          console.log(`Create courses table failed: ${err.message}`);
+        } else {
+          console.log(
+            `Course table created successfull: ${result.affectedRows}`
+          );
         }
       }
     );
--- models/users.js
+++ models/users.js
@@ -1,20 +1,3 @@
-let data = [
-  {
-    id: 1,
-    name: 'idi mohammed',
-    email: 'idi@gmail.com',
-    phone: '123-456-565',
-    address: 'katako laranto',
-  },
-  {
-    id: 2,
-    name: 'mohd mohammed',
-    email: 'mohd@gmail.com',
-    phone: '023-456-865',
-    address: 'bauchi road',
-  },
-];
-
 const conn = require('../helpers/connection');
 
 module.exports = {
@@ -25,36 +8,50 @@ module.exports = {
       values,
       (err, result) => {
         if (err) {
-          callback(null, err);
+          callback(err, null);
         } else {
-          callback(result);
+          callback(null, result);
         }
       }
     );
   },
   find: (callback) => {
-    let users;
     conn.query('SELECT * FROM users', (err, res) => {
       if (err) {
-        callback(null, err);
+        callback(err, null);
       } else {
-        callback(res);
+        callback(null, res);
       }
     });
   },
-  findOne: (id) => data.filter((user) => user.id === id)[0],
-  update: (id, body) => {
-    const user = data.filter((user) => user.id === id)[0];
-    const indexOfUser = data.indexOf(user);
-    const newUser = { ...user, ...body };
-    data[indexOfUser] = newUser;
-    return newUser;
+
+  findOne: (id, callback) => {
+    conn.query('SELECT * FROM users WHERE id = ?', id, (err, res) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, res);
+      }
+    });
   },
 
-  delete: (id) => {
-    data = data.filter((user) => user.id !== id);
-    return {
-      delete: true,
-    };
+  update: (id, body, callback) => {
+    conn.query('UPDATE users SET ? WHERE id = ?', [body, id], (error, res) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, res);
+      }
+    });
+  },
+
+  delete: (id, callback) => {
+    conn.query('DELETE FROM users WHERE id = ?', id, (error, res) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, res);
+      }
+    });
   },
 };
